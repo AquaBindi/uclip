1a2359eee67276e0bfcf7bc76460db07e7d007c9
Moriyama Toshiharu <aqua.bindi@gmail.com>
2024-04-13 21:14:06 +0900

    Remove newline at end of string

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ use clipboard_win::{formats, set_clipboard};
 fn main() {
     let stdin = io::stdin();
     let text = io::read_to_string(stdin).expect("Can not read stdin");
-    set_clipboard(formats::Unicode, text).expect("Can not set on clipboard");
+    set_clipboard(formats::Unicode, text.trim_end()).expect("Can not set on clipboard");
 }
 
